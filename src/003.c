9cc502debfe664bf9015e28a995fd0da3c9a458e
Jake Huneau <jakehuneau@yahoo.com>
2024-10-08 17:56:39 +0000

    problem3

--- src/003.c
+++ src/003.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
+  unsigned long long sol = 13195;//600851475143;
+  unsigned long long i;
   
+  for (i = 2; i < sol / 2; i++) {
+    while (sol % i == 0) {
+      printf("%llu /= %llu -> %llu\n", sol, i, sol / i);
+      sol /= i;
+    }
+  }
+  printf("%llu", sol);
+  return 0;
 }
